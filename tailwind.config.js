127130077ab93cfdd3c99b2fb0ed17cf56312952
Tariq Imtinan <timtinan@outlook.com>
2022-11-01 08:40:42 +0600

    add css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primarySoftOrange: 'hsl(35, 77%, 62%)',
+      primarySoftRed: 'hsl(5, 85%, 63%)',
+      neutralOffWhite: 'hsl(36, 100%, 99%)',
+      neutralGrayishBlue: 'hsl(233, 8%, 79%)',
+      neutralDarkGrayishBlue: 'hsl(236, 13%, 42%)',
+      neutralVeryDarkBlue: 'hsl(240, 100%, 5%)',
+    },
     extend: {},
   },
   plugins: [],
